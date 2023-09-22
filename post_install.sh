bd169838153bd1d8ebfb76b5815a9b3af25591a8
Tayler Geiger <mail@tayler.dev>
2023-09-22 09:24:07 -0500

    Several changes, i.e. starship, move gnome-vrr
    - add starship
    - remove fractional scaling
    - move gnome-vrr version check back to Containerfile

--- post_install.sh
+++ post_install.sh
@@ -17,6 +17,12 @@ mv /tmp/cascadiacode-nerdfont/*.ttf /tmp/cascadiacode-nerdfont/LICENSE /usr/shar
 fc-cache -f /usr/share/fonts/inter && \
 fc-cache -f /usr/share/fonts/intelonemono-nerdfont && \
 
+# install Starship shell prompt
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" && \
+  tar -xzf /tmp/starship.tar.gz -C /tmp && \
+  install -c -m 0755 /tmp/starship /usr/bin && \
+  echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
 # remove CLI app .desktop files and copr repos
 rm -f /usr/share/applications/fish.desktop && \
 rm -f /usr/share/applications/htop.desktop && \
