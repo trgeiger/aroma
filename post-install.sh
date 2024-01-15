6e7b9154b0eb5be61cd4219821aaa81eef39200b
Tayler Geiger <mail@tayler.dev>
2024-01-15 10:46:30 -0600

    Clean up a bunch of stuff - change to nobara kernel - remove more packages like gamemode - change /etc/os-release name - enable ublue-update - fix up default folders

--- post-install.sh
+++ post-install.sh
@@ -16,15 +16,17 @@ fc-cache -f /usr/share/fonts/intelonemono-nerdfont
 
 # install Starship shell prompt
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" && \
-  tar -xzf /tmp/starship.tar.gz -C /tmp && \
-  install -c -m 0755 /tmp/starship /usr/bin && \
-  echo 'eval "$(starship init bash)"' >> /etc/bashrc
+tar -xzf /tmp/starship.tar.gz -C /tmp && \
+install -c -m 0755 /tmp/starship /usr/bin && \
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
 # remove CLI app .desktop files and copr repos
 rm -f /usr/share/applications/fish.desktop && \
 rm -f /usr/share/applications/htop.desktop && \
 rm -f /usr/share/applications/nvtop.desktop && \
 rm -f /usr/share/applications/shredder.desktop && \
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop && \
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/gnome-system-monitor.desktop && \
 rm -rf /etc/yum.repos.d/_copr*
 
 # add Flathub repo
@@ -35,11 +37,15 @@ wget -q https://dl.flathub.org/repo/flathub.flatpakrepo -P /usr/etc/flatpak/remo
 wget https://gitlab.com/popsulfr/steamos-btrfs/-/raw/11114e4ff791eb2c385814c2fcbac6a83f144f35/files/usr/lib/systemd/system/btrfs-dedup@.service -O /usr/lib/systemd/system/btrfs-dedup@.service && \
 wget https://gitlab.com/popsulfr/steamos-btrfs/-/raw/11114e4ff791eb2c385814c2fcbac6a83f144f35/files/usr/lib/systemd/system/btrfs-dedup@.timer -O /usr/lib/systemd/system/btrfs-dedup@.timer
 
-# enable systemd units
+# systemd
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf && \
 systemctl enable com.system76.Scheduler.service && \
 systemctl enable dconf-update.service && \
 systemctl enable ublue-system-flatpak-manager.service && \
 systemctl --global enable ublue-user-flatpak-manager.service
 systemctl enable btrfs-dedup@var-home.timer && \
 systemctl disable rpm-ostreed-automatic.timer && \
-systemctl --global enable podman.socket
+systemctl enable ublue-update.timer && \
+systemctl --global enable podman.socket && \
+sed -i '/^PRETTY_NAME/s/Silverblue/Aroma/' /usr/lib/os-release
